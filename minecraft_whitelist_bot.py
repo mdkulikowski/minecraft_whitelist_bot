9f6f4f5456d4639d80209448ec7d486af3be22f6
mkulik4 <mkulik4@uic.edu>
2023-07-01 21:53:16 -0500

    wrote uuid feature

--- minecraft_whitelist_bot.py
+++ minecraft_whitelist_bot.py
@@ -4,6 +4,7 @@
 import discord
 import json
 import subprocess
+import re
 from dotenv import dotenv_values
 
 ##### data###############
@@ -28,6 +29,9 @@ async def seach_whitelist(name,data,uuid):
             return True
     return False
 
+async def write_to_whitelist(data):
+    with open(whitelist_path, 'w') as file:
+        json.dump(data, file)
 
 async def register_name(message,name,data):
     # check if duplicates
@@ -36,20 +40,44 @@ async def register_name(message,name,data):
         new_data = {}
         new_data['name'] = name
         data.append(new_data)
-        with open(whitelist_path, 'w') as file:
-            json.dump(data, file)
+        write_to_whitelist(data)
         await message.channel.send("Registered user in whitelist...Server will now restart")
         subprocess.call(['bash', script_dir], cwd=working_dir)
     else:
         await message.channel.send('Duplicate name detected, will not restart')
 
 
-async def register_uuid(message,uuid,data):
-    #regex the last login attempt
+async def search_rejection_logs(logs):
+    rejection = re.search('You are not white-listed on this server!', logs)
+    if (rejection != None):
+        pattern = r'UUID of player \S* is \S*'
+        player_info_regex = re.search(pattern, logs)
+        player_info = player_info_regex.group(0).split(' ')
+        return(player_info[3], player_info[5])
+    else:
+        return None
+
+
+async def register_uuid(message,data):
+    #get the last login attempt
+    docker_logs = subprocess.run(['docker', 'logs', '--tail', '7', 'minecraft-server'], capture_output=True, text=True) 
+    error_log = docker_logs.stdout
+    #search for attempt to join the server
+    player_data = search_rejection_logs(error_log)
     #get name and uuid if found
-    #determine if name is registered
-    #write uuid if name found
-    #restart server
+    if (player_data):
+        #determine if name is registered
+        if(seach_whitelist(player_data[0], data, player_data[1])):
+            #write uuid if name found
+            await message.channel.send('Found record in whitelist...registering UUID')
+            write_to_whitelist(data)
+            #restart server
+            await message.channel.send('server restarting')
+            subprocess.call(['bash', script_dir], cwd=working_dir)
+        else:
+            await message.channel.send('No record of player in whitelist. No UUID registered')
+    else: 
+        await message.channel.send('Your login attempt was unable to be found on the server')
     return True
 ###################
 
@@ -68,7 +96,7 @@ async def on_message(message):
         thread_id = env_vars['THREAD_ID']  # Minecraft Whitelist Thread
         if message.channel.id == int(thread_id):
             if message.author != client.user:
-                if message.content.startswith("#"):
+                if message.content.startswith('#'):
                     pass
                 else:
                     user_input = message.content
@@ -77,7 +105,7 @@ async def on_message(message):
                         data = json.load(file)
                     
                     if message.content == 'uuid':
-                        register_uuid(message,user_input,data)
+                        register_uuid(message,data)
                     else:
                         register_name(message,user_input,data)
 
---
+++ restart_whitelist_bot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo systemctl daemon-reload
+sudo systemctl restart minecraft_whitelist_bot.service
